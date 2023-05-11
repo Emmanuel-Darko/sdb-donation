cd53ab2ecc7d3ca621e24235f93afa118db6ed40
Emmanuel Ofosu. Darko <Emmanuel Darko@maximnyansa.org>
2023-05-11 16:27:08 +0100

    working on vue frontend

---
+++ client/src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import LandinPage from './views/LandinPage.vue';
+</script>
+
+<template>
+  <LandinPage/>
+  <RouterView />
+</template>
+
+<style scoped>
+
+</style>
---
+++ client/src/components/NavComponent.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <ul>
+            <li>Home</li>
+            <li>Home</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="css" scoped>
+
+</style>
